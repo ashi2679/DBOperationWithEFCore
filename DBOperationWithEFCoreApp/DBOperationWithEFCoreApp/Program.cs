99f4dcc307a65636eca3917b45b880c7768f14fe
ashi2679 <ashi2679@gmail.com>
2024-12-10 02:15:32 +0530

    commited all

---
+++ DBOperationWithEFCoreApp/DBOperationWithEFCoreApp/Program.cs
@@ -0,0 +1,41 @@
+using DBOperationWithEFCoreApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBOperationWithEFCoreApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<AppDBContext>(option =>
+                option.UseSqlServer(builder.Configuration.GetConnectionString("dbcs")));
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
